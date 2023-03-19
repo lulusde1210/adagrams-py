d3e25f637bbb9ba9e6c8d4d6f887b52c2dfef190
lulu <lulusun1210@gmail.com>
2023-03-18 20:22:38 -0500

    Finish draw_letters() and pass test_wave_01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,47 @@
-def draw_letters():
-    pass
+import random
 
-def uses_available_letters(word, letter_bank):
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+def draw_letters():
+    letter_list = []
+    for (letter, letter_freq) in LETTER_POOL.items():
+        letter_list += [letter] * letter_freq
+
+    letters =[]
+    for i in range(10):
+        letter = letter_list.pop(random.randrange(len(letter_list)))
+        letters.append(letter)
+    return letters
+
+
+def uses_available_letters(word, letter_bank): 
     pass
 
 def score_word(word):
@@ -9,3 +49,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
